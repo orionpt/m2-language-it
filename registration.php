74f682a2640977281253eeeb51f7f57ff41005bb
orionpt <info@orioneurocorp.com>
2022-11-18 16:42:51 +0000

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,19 @@
+
+
+<?php
+/**
+ *                     Spanish package
+ *                     NOTICE OF LICENSE
+ *
+ *                     This source file is subject to the MIT License
+ *                     that is bundled with this package in the file LICENSE.txt.
+ *
+ *                     @package   Spanish
+ *                     @copyright Copyright (c) 2020
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'orionpt_it_it',
+    __DIR__
+);
